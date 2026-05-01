912bdc911a4f1c8ef01448c993550077ff252196
易修然 <yixiuran@163.com>
2026-05-01 21:25:34 +0800

    项目初始化 + 规范配置

---
+++ .storybook/preview.ts
@@ -0,0 +1,14 @@
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                colors: /(background|color)$/i,
+                date: /Date$/i
+            }
+        }
+    }
+}
+
+export default preview
